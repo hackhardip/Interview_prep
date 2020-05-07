92335c13e35af2173ab3c354ff9befb10bb1b9da
Hardip Parmar <hardipparmar1997@gmail.com>
2020-05-07 11:10:18 -0400

    Create 238. Product of Array Except Self.py

---
+++ 238. Product of Array Except Self.py
@@ -0,0 +1,28 @@
+'''
+Given an array nums of n integers where n > 1,  return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+
+Example:
+
+Input:  [1,2,3,4]
+Output: [24,12,8,6]
+Constraint: It's guaranteed that the product of the elements of any prefix or suffix of the array (including the whole array) fits in a 32 bit integer.
+
+Note: Please solve it without division and in O(n).
+
+Follow up:
+Could you solve it with constant space complexity? (The output array does not count as extra space for the purpose of space complexity analysis.)
+'''
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        result = [1] * len(nums)
+        product =1
+        for i in range(len(nums)):
+            result[i] *= product
+            product *= nums[i]
+        product =1
+        for i in range(len(nums)-1,-1,-1):
+            result[i] *= product
+            product*=nums[i]
+        return result
+            
+            
